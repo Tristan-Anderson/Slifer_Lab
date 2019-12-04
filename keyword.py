dfc6891fd40de26f7f75662992edce216401c158
tristan <tris31299@gmail.com>
2019-12-04 16:16:09 -0500

    Pushing executable

--- keyword.py
+++ keyword.py
@@ -2,5 +2,4 @@ from slifercal import slifercal as sc
 
 k = sc()
 
-k.keyword(["mm", "VME", "VNA", "SDR", "curve", "wave"], thermistors=["CCS.F9", "CCS.F10", "CX.T2", "CCX.T1", "CCS.F11", "CCCS.T3"], kelvin=True)
-
+sc.plot_top_n_ranges()
