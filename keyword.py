066a3421b13c61722fd13e6367f05bfbdadc6bf9
tristan <tris31299@gmail.com>
2020-03-21 16:22:57 -0400

    push to server

--- keyword.py
+++ keyword.py
@@ -2,4 +2,4 @@ from slifercal import slifercal as sc
 
 k = sc()
 
-k.keyword(keywords=['vna', 'mm-waves', 'TE', 'Polarization'], persistance=True)
+k.keyword(keywords=['mm', 'waves', 'VNA', 'VNE'], persistance=True)
